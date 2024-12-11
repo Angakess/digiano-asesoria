4ccbfaf4a5685ebea55abd4aeb91fd7e98df3d8f
Andres Kessler <andresgabriel.kessler@gmail.com>
2024-12-11 16:39:52 -0300

    Agrego video de fondo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
-import img1 from "../assets/hero.png";
+import video1 from "../assets/waves.mp4"
 
 export default function Hero() {
   return (
     <section id="inicio" className="relative h-screen overflow-hidden">
-      <div className="absolute inset-0 w-full h-full transition-opacity duration-1000 opacity-100">
-        <img className="object-cover w-full h-full" src={img1}></img>
+      <div className="absolute inset-0 w-full h-full transition-opacity duration-1000 opacity-100 bg-black">
+        <video className="object-cover w-full h-full animate-[fadeOutIn_21.03s_infinite]" src={video1} loop autoPlay ></video>
       </div>
       <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
         <div className="text-center text-white sapce-y-8">
