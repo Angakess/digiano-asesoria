1d5d03359c5ff3962999f3d3faf4234c443173a3
Andres Kessler <andresgabriel.kessler@gmail.com>
2024-12-11 16:58:16 -0300

    Agrego atributo muted para que chrome permita iniciar el video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section id="inicio" className="relative h-screen overflow-hidden">
       <div className="absolute inset-0 w-full h-full transition-opacity duration-1000 opacity-100 bg-black">
-        <video className="object-cover w-full h-full animate-[fadeOutIn_21.03s_infinite]" src={video1} loop autoPlay ></video>
+        <video className="object-cover w-full h-full animate-[fadeOutIn_21.03s_infinite]" src={video1} loop autoPlay muted ></video>
       </div>
       <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
         <div className="text-center text-white sapce-y-8">
